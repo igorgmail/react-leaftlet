ee4dc018f225ded9a156133addd7f14cad424390
Igor.K <igor.k.nsk@gmail.com>
2023-10-26 21:42:58 +0700

    test for mobile

--- src/components/MainCars/lib/config.js
+++ src/components/MainCars/lib/config.js
@@ -1,7 +1,11 @@
+import isHasToushScreen from "./isMobile";
+console.log("▶ ⇛ isHasToushScreen:", isHasToushScreen());
+
 const carsPageconfig = {
   updateDelay: 1 * 1000,
   updatePosMap: 10 * 1000,
   offsetMapPan: 28,
+  differentTime: 5 * 60 * 60 * 1000 // 5 часов
 }
 
 export default carsPageconfig;
